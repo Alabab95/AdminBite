1068e18cdbe48d72248921dae3b36fc885148fdd
Ala <Ala.BenAbdallah@barac.io>
2020-05-06 03:31:06 +0100

    fix conflicts

--- server/routes/package.router.js
+++ server/routes/package.router.js
@@ -1,5 +1,5 @@
 const packages = require('express').Router();
-const ctrlPackage = require('../controllers/package.controller');
+const ctrlPack = require('../controllers/package.controller');
 
 packages.post('/',ctrlPack.createPackage);
 packages.get('/:id',ctrlPack.returnPackage);
@@ -7,6 +7,6 @@ packages.get('/',ctrlPack.allPackages);
 packages.patch('/:id',ctrlPack.update);
 packages.delete('/:id',ctrlPack.delete);
 
-packages.post('/addService',ctrlPackage.addServicesToPackage);
+packages.post('/addService',ctrlPack.addServicesToPackage);
 
 module.exports = packages;
