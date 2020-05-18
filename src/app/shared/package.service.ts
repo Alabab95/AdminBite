91a6366f6f690153ed1f0805d94658710b2016d8
Ala <Ala.BenAbdallah@barac.io>
2020-05-18 05:00:58 +0100

    fix

--- src/app/shared/package.service.ts
+++ src/app/shared/package.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 import { environment } from '../../environments/environment';
-import { Package } from './Package.model';
+import { Package } from './package.model';
 
 @Injectable({
   providedIn: 'root'
